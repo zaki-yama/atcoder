04c87345ba9639ae02309e3ce3bea520c162a6af
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-04-26 01:58:25 +0900

    AC

--- abc087-b/src/main.rs
+++ abc087-b/src/main.rs
@@ -4,16 +4,21 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        a: i32,
+        b: i32,
+        c: i32,
+        x: i32,
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+
+    let mut ans = 0;
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
--- abc087-b/tests/sample_inputs.rs
+++ abc087-b/tests/sample_inputs.rs
@@ -8,40 +8,53 @@ fn sample1() {
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"2
+2
+2
+100"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "2\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"5
+1
+0
+150"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "0\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"30
+40
+50
+6000"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "213\n");
     assert!(output.stderr_str().is_empty());
 }
