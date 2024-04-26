85cd434949c2a7521f5205cefd9b645028f39797
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-04-27 01:10:57 +0900

    AC

--- abc083-b/src/main.rs
+++ abc083-b/src/main.rs
@@ -7,5 +7,16 @@ fn main() {
         (n, a, b): (i32, i32, i32)
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 1..=n {
+        let sum = i
+            .to_string()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
+            .sum::<i32>();
+        if a <= sum && sum <= b {
+            ans += i;
+        }
+    }
+    println!("{}", ans);
 }
