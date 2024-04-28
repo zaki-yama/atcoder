0f056fd0793ec0bc0ce41c78db72e8f39147490c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-04-29 00:10:00 +0900

    ABC088B - Card Game for Two (#7)

---
+++ abc088-b/tests/sample_inputs.rs
@@ -0,0 +1,54 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"2
+3 1"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "2\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"3
+2 7 4"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "5\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"4
+20 18 2 18"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "18\n");
+    assert!(output.stderr_str().is_empty());
+}
