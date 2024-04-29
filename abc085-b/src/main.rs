babf35733bfa9022ece5b7767ce859332eaf8fd5
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-04-29 21:29:58 +0900

    AC

--- abc085-b/src/main.rs
+++ abc085-b/src/main.rs
@@ -1,19 +1,16 @@
 // -*- coding:utf-8-unix -*-
 
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        d: [[i32; 1]; n],
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+
+    let remove_duplicate: HashSet<i32> = d.into_iter().flat_map(|x| x).collect();
+
+    println!("{:?}", remove_duplicate.len());
 }
--- abc085-b/tests/sample_inputs.rs
+++ abc085-b/tests/sample_inputs.rs
@@ -8,40 +8,58 @@ fn sample1() {
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"4
+10
+8
+8
+6"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "3\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"3
+15
+15
+15"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "1\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"7
+50
+30
+50
+100
+50
+80
+30"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "4\n");
     assert!(output.stderr_str().is_empty());
 }
