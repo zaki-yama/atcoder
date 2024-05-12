1abfea92eaf1048e9118742eafde65a77d21c064
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-05-12 23:28:34 +0900

    ABC046 B - AtCoDeerくんとボール色塗り (#17)

---
+++ abc046-b/tests/sample_inputs.rs
@@ -0,0 +1,45 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin("2 2")
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "2\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin("1 10")
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "10\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin("10 8")
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "322828856\n");
+    assert!(output.stderr_str().is_empty());
+}
