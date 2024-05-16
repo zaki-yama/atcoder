1927da98620625ac962b84336ff57223bee566d9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-05-17 06:52:54 +0900

    wip

--- abc048-b/src/main.rs
+++ abc048-b/src/main.rs
@@ -2,16 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        a: u64,
+        b: u64,
+        x: u64,
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+
+    let mut ans = 0;
+    let mut tmp = x;
+    while tmp <= b {
+        if tmp >= a {
+            ans += 1;
+        }
+        tmp += x;
+    }
+    println!("{}", ans);
 }
--- abc048-b/tests/sample_inputs.rs
+++ abc048-b/tests/sample_inputs.rs
@@ -8,40 +8,52 @@ fn sample1() {
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin("4 8 2")
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "3\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin("0 5 1")
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "6\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin("9 9 2")
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "0\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin("1 1000000000000000000 3")
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "333333333333333333\n");
     assert!(output.stderr_str().is_empty());
 }
