b4f6952995b4f55927295bef5c8cc65d49ddf2be
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-06-14 01:20:51 +0900

    AC

--- abc355-b/src/main.rs
+++ abc355-b/src/main.rs
@@ -7,10 +7,11 @@ fn main() {
         mut a: [i32; n],  // Vec<(i32, i32, i32)>
         mut b: [i32; m],  // Vec<(i32, i32, i32)>
     }
-    a.append(&mut b);
-    a.sort();
-    for w in a.windows(2) {
-        if a.contains(&w[0]) == a.contains(&w[1]) {
+    let mut c = a.clone();
+    c.append(&mut b);
+    c.sort();
+    for w in c.windows(2) {
+        if a.contains(&w[0]) && a.contains(&w[1]) {
             println!("Yes");
             return;
         }
