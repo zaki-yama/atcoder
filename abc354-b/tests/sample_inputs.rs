32c768f905e1e9b98750d5659ab4e160692d31cd
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-06-17 21:42:15 +0900

    AC

---
+++ abc354-b/tests/sample_inputs.rs
@@ -0,0 +1,41 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"3
+takahashi 2
+aoki 6
+snuke 5"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "snuke\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"3
+takahashi 2813
+takahashixx 1086
+takahashix 4229"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "takahashix\n");
+    assert!(output.stderr_str().is_empty());
+}
