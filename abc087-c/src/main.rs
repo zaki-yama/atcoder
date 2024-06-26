1112fc7c21ee62b662bdccca8f648099ea2a13ac
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-06-27 06:50:16 +0900

    AC

--- abc087-c/src/main.rs
+++ abc087-c/src/main.rs
@@ -3,15 +3,28 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        board: [[i32; n]; 2],
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+    let mut candies = vec![];
+
+    for i in 0..n {
+        // println!("{}", i);
+        let mut sum = 0;
+        for j in 0..i {
+            // println!("1: {}", j);
+            sum += board[0][j];
+        }
+
+        sum += board[0][i];
+        sum += board[1][i];
+
+        for j in i + 1..n {
+            // println!("2: {}", j);
+            sum += board[1][j];
+        }
+
+        candies.push(sum);
+    }
+
+    println!("{}", candies.iter().max().unwrap());
 }
--- abc087-c/tests/sample_inputs.rs
+++ abc087-c/tests/sample_inputs.rs
@@ -8,40 +8,68 @@ fn sample1() {
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"5
+3 2 2 4 1
+1 2 2 2 1"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "14\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"4
+1 1 1 1
+1 1 1 1"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "5\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
-#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         // change here
-        .output_with_stdin(r#" "#)
+        .output_with_stdin(
+            r#"7
+3 3 4 5 4 5 3
+5 3 4 4 2 3 2"#,
+        )
         .tee_output()
         .expect_success();
     // change here
-    assert_eq!(output.stdout_str(), "\n");
+    assert_eq!(output.stdout_str(), "29\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"1
+2
+3"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "5\n");
     assert!(output.stderr_str().is_empty());
 }
