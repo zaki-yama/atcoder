e4cd0ee6e2bb5e78f86da830b99ee6c551435739
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-09-21 22:35:38 +0900

    ABC372 C - Count ABC Again

--- abc372/abc372-c/src/main.rs
+++ abc372/abc372-c/src/main.rs
@@ -7,16 +7,58 @@ fn main() {
         mut s: String,
         x_c: [(usize, char); q],
     }
-    // let mut ans = Vec::new();
+    let original_abc_count = s.matches("ABC").count();
+    let mut count = original_abc_count;
+    let mut chars: Vec<char> = s.chars().collect();
+
+
     for i in 0..q {
         let query = x_c[i];
-        s = s
-            .chars()
-            .enumerate()
-            .map(|(i, ch)| if i == query.0 - 1 { query.1 } else { ch })
-            .collect();
-        // println!("{}: {}", i, s);
-        let abc_count = s.matches("ABC").count();
-        println!("{}", abc_count);
+        let x = query.0 - 1;
+        let c = query.1;
+
+        let target = chars[x];
+
+        match target {
+            'A' => {
+                if x < n - 2 && chars[x + 1] == 'B' && chars[x + 2] == 'C' {
+                    count -= 1;
+                }
+            }
+            'B' => {
+                if x > 0 && x < n - 1 && chars[x - 1] == 'A' && chars[x + 1] == 'C' {
+                    count -= 1;
+                }
+            }
+            'C' => {
+                if x > 1 && chars[x - 2] == 'A' && chars[x - 1] == 'B' {
+                    count -= 1;
+                }
+            }
+            _ => {}
+        }
+
+        match c {
+            'A' => {
+                if x < n - 2 && chars[x + 1] == 'B' && chars[x + 2] == 'C' {
+                    count += 1;
+                }
+            }
+            'B' => {
+                if x > 0 && x < n - 1 && chars[x - 1] == 'A' && chars[x + 1] == 'C' {
+                    count += 1;
+                }
+            }
+            'C' => {
+                if x > 1 && chars[x - 2] == 'A' && chars[x - 1] == 'B' {
+                    count += 1;
+                }
+            }
+            _ => {}
+        }
+
+        println!("{}", count);
+
+        chars[x] = c;
     }
 }
