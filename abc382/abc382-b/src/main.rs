8fb3b13136182421402f95b3cafe13c4b817df00
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-12-01 22:02:01 +0900

    ABC283 B - Cookies2 (better solution)

--- abc382/abc382-b/src/main.rs
+++ abc382/abc382-b/src/main.rs
@@ -6,30 +6,16 @@ fn main() {
         d: usize,
         s: String,
     }
-    let cookies = s.chars().filter(|&ch| ch == '@').count();
-    let cookies_rest = cookies - d;
-    if cookies_rest == 0 {
-        println!("{}", ".".repeat(n));
-        return;
-    }
-    let index = find_last_cookie_index(&s, n, cookies_rest);
-    let head: String = s.chars().take(index).collect();
-    let dots = ".".repeat(n - index);
+    let mut chars: Vec<char> = s.chars().collect();
 
-    println!("{}{}", head, dots);
-}
-
-fn find_last_cookie_index(s: &str, n: usize, num_cookies: usize) -> usize {
-    let mut chars = s.chars();
-    let mut count = 0;
-    for i in 0..n {
-        let ch = chars.nth(0).unwrap();
-        if ch == '@' {
-            count += 1;
-            if count > num_cookies {
-                return i;
-            }
+    for _ in 0..d {
+        if let Some(pos) = chars.iter().rposition(|&ch| ch == '@') {
+            chars[pos] = '.';
+        } else {
+            break;
         }
     }
-    return n - 1;
+
+    let result: String = chars.iter().collect();
+    println!("{}", result);
 }
--- abc382/abc382-b/tests/sample_inputs.rs
+++ abc382/abc382-b/tests/sample_inputs.rs
@@ -20,7 +20,6 @@ fn sample1() {
 }
 
 #[test]
-#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
@@ -38,7 +37,6 @@ fn sample2() {
 }
 
 #[test]
-#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
