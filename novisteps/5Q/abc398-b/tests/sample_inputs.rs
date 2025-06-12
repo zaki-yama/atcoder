f01fb69e6b28762dee9f2061cdd603af7344a92d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2025-06-13 07:03:45 +0900

    tmp

---
+++ novisteps/5Q/abc398-b/tests/sample_inputs.rs
@@ -0,0 +1,62 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(r#"1 4 1 4 2 1 3"#)
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "Yes\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+#[ignore]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(r#"11 12 13 10 13 12 11"#)
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "No\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+#[ignore]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(r#"7 7 7 7 7 7 7"#)
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "No\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+#[ignore]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(r#"13 13 1 1 7 4 13"#)
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "Yes\n");
+    assert!(output.stderr_str().is_empty());
+}
