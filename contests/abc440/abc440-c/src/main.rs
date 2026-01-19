280563ba4bd348fd75141e5a375559286f7983be
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2026-01-20 04:47:32 +0900

    ABC440 C (AC)

--- contests/abc440/abc440-c/src/main.rs
+++ contests/abc440/abc440-c/src/main.rs
@@ -4,24 +4,33 @@ fn main() {
     input! {
         t: usize,
     }
-    for _ in 0..t {
+    for f in 0..t {
         input! {
-            N: u64,
-            W: u64,
+            N: usize,
+            W: usize,
             C: [u64; N]
         }
-        let mut min = u64::MAX;
-        for x in 0..2 * W {
-            let mut cost = 0;
-            for i in 0..N {
-                if (i + x) % (2 * W) < W {
-                    cost += C[i as usize];
-                }
-            }
-            if cost < min {
-                min = cost;
+
+        // 長さ2Wのコスト配列を作る
+        let mut sum_costs = vec![0; 2 * W];
+
+        for i in 0..N {
+            sum_costs[i % (2 * W)] += C.get(i).copied().unwrap_or(0);
+        }
+
+        // 初期値は 0..W
+        let mut current: u64 = sum_costs[0..W].iter().sum();
+        let mut ans = current;
+
+        for i in 1..2 * W {
+            // i 番目は、current + C[i+W-1] - C[i-1]
+            current += sum_costs[(i + W - 1) % (2 * W)];
+            current -= sum_costs[i - 1];
+            if ans > current {
+                ans = current;
             }
         }
-        println!("{}", min);
+
+        println!("{}", ans);
     }
 }
