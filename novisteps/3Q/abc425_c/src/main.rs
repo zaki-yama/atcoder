1db4c6e2b152772539c84ffd63b51e23ca81a6bf
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2026-05-01 01:53:28 +0900

    ABC425 C - Rotate and Sum Query

--- novisteps/3Q/abc425_c/src/main.rs
+++ novisteps/3Q/abc425_c/src/main.rs
@@ -7,6 +7,13 @@ fn main() {
         a: [u64; n],
     }
 
+    let mut s = vec![0; n];
+    s[0] = a[0];
+    for i in 1..n {
+        s[i] = s[i - 1] + a[i];
+    }
+    // println!("{:?}", s);
+
     let mut cursor = 0;
     for i in 0..q {
         // println!("---------{}", i);
@@ -32,10 +39,11 @@ fn main() {
 
                 let l = (l + cursor - 1) % n;
                 let r = (r + cursor - 1) % n;
+                let sl = if l == 0 { 0 } else { s[l - 1] };
                 let sum: u64 = if l <= r {
-                    a[l..=r].iter().copied().sum()
+                    s[r] - sl
                 } else {
-                    a[l..n].iter().copied().sum::<u64>() + a[0..=r].iter().copied().sum::<u64>()
+                    s[n - 1] - sl + s[r]
                 };
                 println!("{}", sum);
             }
