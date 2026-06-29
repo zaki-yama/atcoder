ecf3d609cd014cd8ac9382368da57c67d99cad0e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2026-06-30 02:26:55 +0900

    ABC408 C - Not All Covered

---
+++ novisteps/3Q/abc408_c/tests/sample_inputs.rs
@@ -0,0 +1,70 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"10 4
+1 6
+4 5
+5 10
+7 10
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "1\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"5 2
+1 2
+3 4
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "0\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        // change here
+        .output_with_stdin(
+            r#"5 10
+2 5
+1 5
+1 2
+2 4
+2 2
+5 5
+2 4
+1 2
+2 2
+2 3
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    // change here
+    assert_eq!(output.stdout_str(), "3\n");
+    assert!(output.stderr_str().is_empty());
+}
